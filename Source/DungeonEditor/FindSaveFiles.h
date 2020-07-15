8daeba53b24bd9943171da819d0c1b91b8b63544
GCJOJO <39382270+JOJOMARIO2007@users.noreply.github.com>
2020-07-15 21:33:52 +0200

    Some Random Commit

---
+++ Source/DungeonEditor/FindSaveFiles.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FindSaveFiles.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONEDITOR_API UFindSaveFiles : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
