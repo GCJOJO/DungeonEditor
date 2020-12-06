10fb7747724b1c0d287754b501da85b75f7d09cc
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2020-12-06 17:59:08 +0100

    Added a Wooden Door + torch on wall

---
+++ Source/DungeonEditor/DiscordLibrary.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DiscordLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONEDITOR_API UDiscordLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		UFUNCTION(BlueprintCallable, Category = "Dungeons", meta = (Keywords = "Init Discord"))
+		static bool InitDiscord();
+
+		UFUNCTION(BlueprintCallable, Category = "Dungeons", meta = (Keywords = "Update Discord"))
+		static bool UpdateDiscord();
+
+		UFUNCTION(BlueprintCallable, Category = "Dungeons", meta = (Keywords = "Discord Set Activity"))
+		static bool SetDiscordActivity(FString State, FString Details);
+	
+};
