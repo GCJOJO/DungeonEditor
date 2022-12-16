08cd7d1fb998801fb17916aad1d5213f0e60cfe6
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2022-12-16 17:43:30 +0100

    Story Mode 1st map (WIP)

--- Source/DungeonEditor/Public/DungeonEditorTypes.h
+++ Source/DungeonEditor/Public/DungeonEditorTypes.h
@@ -14,11 +14,11 @@
 //													Editor Mode | Test Mode in Editor | Basic Play mode (no Editor)
 UENUM(Blueprintable, BlueprintType) enum GameModeTypes { Editor, PlayModeEditor, PlayMode };
 UENUM(Blueprintable, BlueprintType) enum ObjectTypes { Basic, Systems, Objects, Props };
-UENUM(BlueprintType, BlueprintType) enum ELocalizationCulture { EN,	FR };
+UENUM(Blueprintable, BlueprintType) enum ELocalizationCulture { EN,	FR };
+UENUM(Blueprintable, BlueprintType) enum EActivatorMethods { Base, FlipFlop };
 
 UCLASS()
 class DUNGEONEDITOR_API UDungeonEditorTypes : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
 };
