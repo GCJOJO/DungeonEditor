9387f8185d3e0682acbf2faf2f697eb027f5ecf2
JOJO <39382270+GCJOJO@users.noreply.github.com>
2025-10-17 11:36:15 +0200

    Updated to UE 5.6 + fixes
    Fixes to Key Doors not saving their required items
    Fixes to new Input System

--- Source/DungeonEditor.Target.cs
+++ Source/DungeonEditor.Target.cs
@@ -8,8 +8,8 @@ public class DungeonEditorTarget : TargetRules
 	public DungeonEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
 
-		ExtraModuleNames.AddRange( new string[] { "DungeonEditor" } );
+        ExtraModuleNames.AddRange( new string[] { "DungeonEditor" } );
 	}
 }
--- Source/DungeonEditorEditor.Target.cs
+++ Source/DungeonEditorEditor.Target.cs
@@ -7,10 +7,9 @@ public class DungeonEditorEditorTarget : TargetRules
 {
 	public DungeonEditorEditorTarget(TargetInfo Target) : base(Target)
 	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-		bOverrideBuildEnvironment = true;
-        ExtraModuleNames.AddRange( new string[] { "DungeonEditor" } );
-	}
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+
+        ExtraModuleNames.AddRange(new string[] { "DungeonEditor" });
+    }
 }
